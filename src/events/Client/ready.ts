57bea04ed31c9ff235cd9c8e9a2b8e8698210262
T.F.A <92172698+TFAGaming@users.noreply.github.com>
2023-03-13 14:45:27 +0100

    Update ready.ts

--- src/events/Client/ready.ts
+++ src/events/Client/ready.ts
@@ -1,19 +1,20 @@
 import { client } from '../../index';
-import { client_config } from '../../config/client';
+import { client_presence_config } from '../../config/client';
+import { PresenceUpdateStatus } from 'discord.js';
 
 client.once('ready', async () => {
     console.log('Logged in as ' + client.user.tag + '.');
 
     setInterval(async () => {
-        const activity_chosen = client_config.activities[Math.floor(Math.random() * client_config.activities.length)];
+        const activity_chosen = client_presence_config.activities[Math.floor(Math.random() * client_presence_config.activities.length)];
 
         client.user.setPresence({
-            status: 'online',
+            status: activity_chosen.status ? PresenceUpdateStatus[activity_chosen.status] : 'online',
             activities: [{
                 name: activity_chosen.name,
                 type: activity_chosen.type,
-                url: activity_chosen.type === 1 ? client_config.twitch_url ? client_config.twitch_url : 'https://www.twitch.tv/discord/' : null
+                url: activity_chosen.type === 1 ? client_presence_config.twitch_url ? client_presence_config.twitch_url : 'https://www.twitch.tv/discord/' : null
             }]
         });
-    }, client_config.change_presence_every_ms && typeof client_config.change_presence_every_ms === 'number' ? client_config.change_presence_every_ms : 7500);
+    }, 7500);
 });
